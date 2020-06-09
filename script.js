97dc36fbc3b015314f092173bb77032efd4de9b7
diamo <rezanc@mail.ru>
2020-06-09 16:18:36 +0400

    запилила адаптив)

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
-    //прикрепляем клик по заголовкам acc-head
-    $('#accordeon .acc-head').on('click', showBody);
+    $('.card-header').click(function (e) {
+        e.preventDefault();
+        $(this).find('.arrows-rotate').toggleClass('arrows-rotate_up');
+    });
+    function bootstrapClearButton() {
+        $('.form-group :input').on('keydown focus', function() {
+            if ($(this).val().length > 0) {
+                $(this).nextAll('.block-clear').removeClass('d-none');
+            }
+        }).on('keydown keyup blur', function() {
+            if ($(this).val().length === 0) {
+                $(this).nextAll('.block-clear').addClass('d-none');
+            }
+        });
+        $('.block-clear').on('click', function() {
+            $(this).addClass('d-none').prevAll(':input').val('');
+        });
+    }
+    bootstrapClearButton();
 });
 
-function showBody(target){
-    $(this).find('.arrow_show').toggleClass('active');
-    $(this).next().slideToggle(500);
-}
